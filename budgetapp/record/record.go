94f983de61f0336fd4338062ad2015293dad3b50
nicholashibberd <nicholashibberd@gmail.com>
2014-11-14 13:14:17 +0000

    IN PROGRESS

--- budgetapp/record/record.go
+++ budgetapp/record/record.go
@@ -1,13 +1,41 @@
 package record
 
+import (
+	"time"
+)
+
 type Record struct {
-	Id int64 `json:"id"`
-	DatastoreRecord
+	Id               int64     `json:"id"`
+	Date             time.Time `json:"date"`
+	Description      string    `json:"description"`
+	Amount           string    `json:"amount"`
+	Balance          string    `json:"balance"`
+	Account_number   string    `json:"account_number"`
+	Transaction_type string    `json:"transaction_type"`
+	Tags             []Tag     `json:"tags"`
 }
 
-func NewRecord(dr DatastoreRecord, k int64) Record {
-	return Record{
-		DatastoreRecord: dr,
-		Id:              k,
+func NewRecord(de string, ac string, am string, da time.Time, b string, t string) Record {
+	tag1 := NewTag("Cash")
+	tag2 := NewTag("Presents")
+	tags := []Tag{tag1, tag2}
+	return DatastoreRecord{
+		Description:      de,
+		Account_number:   ac,
+		Amount:           am,
+		Date:             da,
+		Balance:          b,
+		Transaction_type: t,
+		Tags:             tags,
+	}
+}
+
+type Tag struct {
+	Name string
+}
+
+func NewTag(name string) Tag {
+	return Tag{
+		Name: name,
 	}
 }
