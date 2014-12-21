c847db978d5e4f41856b8a831d279fa45cf19490
nicholashibberd <nicholashibberd@gmail.com>
2014-12-21 20:34:49 +1100

    Formatting currency properly

--- public/javascripts/views/records-summary-view.js
+++ public/javascripts/views/records-summary-view.js
@@ -8,13 +8,19 @@ var app = app || {};
     render: function() {
       this.$el.text(this.total());
     },
-    formatCurrency: function(amount) {
-      return "$" + amount.toFixed(2).replace(/./g, function(c, i, a) {
-        return i && c !== "." && !((a.length - i) % 3) ? ',' + c : c;
-      });
+    formatMoney: function(number, places, symbol, thousand, decimal) {
+      number = number || 0;
+      places = !isNaN(places = Math.abs(places)) ? places : 2;
+      symbol = symbol !== undefined ? symbol : "$";
+      thousand = thousand || ",";
+      decimal = decimal || ".";
+      var negative = number < 0 ? "-" : "",
+      i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+      j = (j = i.length) > 3 ? j % 3 : 0;
+      return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
     },
     total: function() {
-      return this.formatCurrency(this.collection.total());
+      return this.formatMoney(this.collection.total());
     }
   })
 })(jQuery);
