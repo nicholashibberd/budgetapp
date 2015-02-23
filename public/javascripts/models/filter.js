ff8658d43bbf9cd0bb4315a45edddc30593ef190
nicholashibberd <nicholashibberd@gmail.com>
2015-02-24 09:57:03 +1100

    Filtering by untagged records

--- public/javascripts/models/filter.js
+++ public/javascripts/models/filter.js
@@ -15,9 +15,13 @@ var app = app || {};
       var records = _.filter(records, function(record) {
         var hasTag = true;
         if (tags !== "all") {
-          var hasTag = _.find(record.tags.models, function(tag) {
-            return tag.get('Name') == tags;
-          });
+          if (tags == "Untagged") {
+            hasTag = record.tags.length == 0
+          } else {
+            hasTag = _.find(record.tags.models, function(tag) {
+              return tag.get('Name') == tags;
+            });
+          }
         }
         var belongsToAccount = accountNumbers.indexOf(record.get('account_number')) !== -1
         return hasTag && belongsToAccount
